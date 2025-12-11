a140fb14098a2ff1da6472e0e37050df9f5d7eda
KarlJowd <carljude.utrera@msugensan.edu.ph>
2025-12-12 05:32:46 +0800

    IDX: Run database migrations before starting server

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,7 +7,7 @@
     "ms-python.python"
   ];
   idx.workspace.onCreate = {
-    create-venv = "python -m venv .venv && .venv/bin/pip install -r requirements.txt";
+    create-venv = "python -m venv .venv && .venv/bin/pip install -r requirements.txt && .venv/bin/python manage.py migrate";
   };
   idx.previews = {
     previews = {
@@ -15,7 +15,7 @@
         command = [
           "sh"
           "-c"
-          "if [ ! -d .venv ]; then python -m venv .venv && .venv/bin/pip install -r requirements.txt; fi && .venv/bin/python manage.py runserver 0.0.0.0:$PORT --noreload --insecure"
+          "if [ ! -d .venv ]; then python -m venv .venv && .venv/bin/pip install -r requirements.txt; fi && .venv/bin/python manage.py migrate --noinput && .venv/bin/python manage.py runserver 0.0.0.0:$PORT --noreload --insecure"
         ];
         manager = "web";
       };
